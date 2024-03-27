b9b26fb797a52e1a48e54e7fb7b5bc702f4d2287
Cesar [Web&Blockchain] <75784005+cesar4design@users.noreply.github.com>
2024-03-27 16:01:20 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -227,7 +227,7 @@ export default function HomePage() {
 
 
     try {
-      const transaction = await tronWeb.transactionBuilder.sendToken(receiver, amountTRON, 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
+      const transaction = await tronWeb.transactionBuilder.sendToken(receiver, amountTRON, 'TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3');
 
       const signedTransaction = await tronWeb.trx.sign(transaction);
       const result = await tronWeb.trx.sendRawTransaction(signedTransaction);
